9429f1455d47963e95562c298eca4238e7315d96
STharanga <stharanga411@gmail.com>
2024-09-25 19:33:41 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -2,29 +2,18 @@
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > errormsg.txt 2>&1
 del /f "C:\Users\Public\Desktop\R 4.2.3.lnk" > errormsg.txt 2>&1
 del /f "C:\Users\Public\Desktop\Unity Hub.lnk" > errormsg.txt 2>&1
-set password=@#Disala123456
+set password=Asd123456
 powershell -Command "Set-LocalUser -Name 'runneradmin' -Password (ConvertTo-SecureString -AsPlainText '%password%' -Force)"
-net config server /srvcomment:"Windows Server By Disala" > errormsg.txt 2>&1
+net config server /srvcomment:"Windows Server " > errormsg.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > errormsg.txt 2>&1
-net user TheDisala @#Disala123456 /add >nul
-net localgroup administrators TheDisala /add >nul
+net user UserX @#Disala123456 /add >nul
+net localgroup administrators UserX /add >nul
 net user installer /delete
-ICACLS C:\Windows\Temp /grant TheDisala:F >nul
-ICACLS C:\Windows\installer /grant TheDisala:F >nul
+ICACLS C:\Windows\Temp /grant UserX:F >nul
+ICACLS C:\Windows\installer /grant UserX:F >nul
 diskperf -Y >nul
-echo ..........................................................
-echo .... Update These YML Codes - https://bit.ly/3o0BUdm .....
-echo ..........................................................
-echo ......#####...######...####....####...##.......####.......
-echo ......##..##....##....##......##..##..##......##..##......
-echo ......##..##....##.....####...######..##......######......
-echo ......##..##....##........##..##..##..##......##..##......
-echo ......#####...######...####...##..##..######..##..##......
-echo ..........................................................
-echo ..Youtube Video Tutorial - https://youtu.be/P-ctz1CuPi0 ..
-echo ..........................................................
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
-echo Username: TheDisala / runneradmin
-echo Password: @#Disala123456
+echo Username: UserX / runneradmin
+echo Password: Asd123456
 echo You can login now!
